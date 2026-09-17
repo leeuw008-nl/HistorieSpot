a76013a63ae89527ce2843858a825219835bbcde
leeuw008-nl <leeuw008@gmail.com>
2026-09-17 11:51:08 +0200

    Restore working app.js from known-good baseline

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // OUDE SITUATIE ZONDER MONUMENTEN - gefixed voor 4 punten
 const map=L.map("map",{zoomControl:false}).setView([52.516,6.42],15);
 window.map=map;
+
+L.control.zoom({position:'bottomleft'}).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{maxZoom:19}).addTo(map);
